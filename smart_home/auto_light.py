6dbee34d68d8b4c7d7435bdd4a4a92afcee85d32
Yin TAN <yin.tan@imt-atlantique.net>
2022-03-22 14:34:16 +0100

    Update auto_light.py

--- smart_home/auto_light.py
+++ smart_home/auto_light.py
@@ -10,56 +10,66 @@ import asyncio
 
 helpers.set_console_title("xaal-dumper")
 
-addr_kitchen_zone = "9963210a-d5ba-11eb-aaa7-cc483ac19e2d"
-addr_bathroom_zone = "9d268b24-d5ba-11eb-aaa7-cc483ac19e2d"
-addr_sofa_pressure = "039593e0-d3ff-11eb-ba59-fc44827cb3d9"
-addr_bed_pressure = "0df9219e-d3ff-11eb-ba59-fc44827cb3d9"
+ADDR_KITCHEN_ZONE = "9963210a-d5ba-11eb-aaa7-cc483ac19e2d"
+ADDR_BATHROOM_ZONE = "9d268b24-d5ba-11eb-aaa7-cc483ac19e2d"
+ADDR_SOFA_PRESSURE = "039593e0-d3ff-11eb-ba59-fc44827cb3d9"
+ADDR_BED_PRESSURE = "0df9219e-d3ff-11eb-ba59-fc44827cb3d9"
 
-addr_light_kitchen = "ccc44227-d4fc-46eb-8578-159e2c47da04"
-addr_light_bathroom = "ccc44227-d4fc-46eb-8578-159e2c47da06"
-addr_screen = "2d42a742-aa2f-11e9-ac3b-a4badbf92501"
-addr_shutter = "2fe70f46-3ece-44d1-af34-2d82e10fb854"
+ADDR_LIGHT_KITCHEN = "ccc44227-d4fc-46eb-8578-159e2c47da04"
+ADDR_LIGHT_BATHROOM = "ccc44227-d4fc-46eb-8578-159e2c47da06"
+ADDR_SCREEN = "2d42a742-aa2f-11e9-ac3b-a4badbf92501"
+ADDR_SHUTTER = "2fe70f46-3ece-44d1-af34-2d82e10fb854"
 
-duration_tv = 5 * 60
-duration_light = 1 * 60
-duration_shutter = 1 * 60
+DURATION_TV = 1 * 5
+DURATION_LIGHT_KITCHEN = 1 * 5
+DURATION_LIGHT_BATHROOM = 1 * 5
+DURATION_SHUTTER = 1 * 10
 
 
 def display(msg):
     print(msg.body.keys())
 
 
-async def get_value_presence(msg):
-    if str(msg.source) == addr_kitchen_zone:
+async def light_kitchen_auto(msg):
+    if str(msg.source) == ADDR_KITCHEN_ZONE:
         if "presence" in msg.body.keys():
             # print(msg.body["presence"])
             # print(type(msg.body["presence"]))
             if msg.body["presence"]:
-                eng.send_request(dev, [tools.get_uuid(addr_light_kitchen)], "turn_on")
+                eng.send_request(dev, [tools.get_uuid(ADDR_LIGHT_KITCHEN)], "turn_on")
             else:
-                await asyncio.sleep(duration_light)
-                eng.send_request(dev, [tools.get_uuid(addr_light_kitchen)], "turn_off")
-    elif str(msg.source) == addr_bathroom_zone:
+                await asyncio.sleep(DURATION_LIGHT_KITCHEN)
+                eng.send_request(dev, [tools.get_uuid(ADDR_LIGHT_KITCHEN)], "turn_off")
+
+
+async def light_bathroom_auto(msg):
+    if str(msg.source) == ADDR_BATHROOM_ZONE:
         if "presence" in msg.body.keys():
             if msg.body["presence"]:
-                eng.send_request(dev, [tools.get_uuid(addr_light_bathroom)], "turn_on")
+                eng.send_request(dev, [tools.get_uuid(ADDR_LIGHT_BATHROOM)], "turn_on")
             else:
-                await asyncio.sleep(duration_light)
-                eng.send_request(dev, [tools.get_uuid(addr_light_bathroom)], "turn_off")
-    elif str(msg.source) == addr_sofa_pressure:
+                await asyncio.sleep(DURATION_LIGHT_BATHROOM)
+                eng.send_request(dev, [tools.get_uuid(ADDR_LIGHT_BATHROOM)], "turn_off")
+
+
+async def turn_tv_auto(msg):
+    if str(msg.source) == ADDR_SOFA_PRESSURE:
         if "detected" in msg.body.keys():
             if msg.body["detected"]:
-                eng.send_request(dev, [tools.get_uuid(addr_screen)], "turn_on")
+                eng.send_request(dev, [tools.get_uuid(ADDR_SCREEN)], "turn_on")
             else:
-                await asyncio.sleep(duration_tv)
-                eng.send_request(dev, [tools.get_uuid(addr_screen)], "turn_off")
-    elif str(msg.source) == addr_bed_pressure:  # not tested
+                await asyncio.sleep(DURATION_TV)
+                eng.send_request(dev, [tools.get_uuid(ADDR_SCREEN)], "turn_off")
+
+
+async def turn_shutter_auto(msg):
+    if str(msg.source) == ADDR_BED_PRESSURE:
         if "detected" in msg.body.keys():
             if msg.body["detected"]:
-                eng.send_request(dev, [tools.get_uuid(addr_screen)], "down")
+                eng.send_request(dev, [tools.get_uuid(ADDR_SHUTTER)], "down")
             else:
-                await asyncio.sleep(duration_shutter)
-                eng.send_request(dev, [tools.get_uuid(addr_screen)], "up")
+                await asyncio.sleep(DURATION_SHUTTER)
+                eng.send_request(dev, [tools.get_uuid(ADDR_SHUTTER)], "up")
 
 
 def main():
@@ -69,7 +79,12 @@ def main():
         dev = devices.basic()
         eng = AsyncEngine()
         eng.add_device(dev)
-        eng.subscribe(get_value_presence)
+
+        eng.subscribe(light_kitchen_auto)
+        eng.subscribe(light_bathroom_auto)
+        eng.subscribe(turn_tv_auto)
+        eng.subscribe(turn_shutter_auto)
+
         eng.disable_msg_filter()
         eng.run()
     except KeyboardInterrupt:
